19e8bd21a812490a1e52dcc59c428a92bba9eb18
eylaing <liz.damaren@gmail.com>
2017-10-21 21:23:10 -0400

    adding nav bar files

--- app/src/main/java/com/example/apomah/bestfriendsfinder/MainActivity.java
+++ app/src/main/java/com/example/apomah/bestfriendsfinder/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.apomah.bestfriendsfinder;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    setContentView(R.layout.activity_main);
+                    return true;
+                case R.id.navigation_dashboard:
+                    setContentView(R.layout.about_layout);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
